1c89e8164cd61b5b26af2fa47738e4604b53d82e
ilandroxxy <ilyand98@mail.ru>
2019-03-25 09:21:24 +0700

    new programm

--- main.cpp
+++ main.cpp
@@ -1,124 +1,53 @@
-#include <SFML/Graphics.hpp>
-#include <time.h>
-using namespace sf;
+#include <iostream>
+#include <cstdlib>
+#include <ostream>
+#include <fstream>
 
+using namespace std;
 
-int size = 56;
-
-Sprite f[32]; //figures
-
-int board[8][8] =
-{
- -1,-2,-3,-4,-5,-3,-2,-1,
- -6,-6,-6,-6,-6,-6,-6,-6,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  6, 6, 6, 6, 6, 6, 6, 6,
-  1, 2, 3, 4, 5, 3, 2, 1 
-};
-
-
-std::string toChessNote(Vector2f p)
-{
-	std::string s = "";
-	s += char(p.x / size + 97);
-	s += char(7 - p.y / size + 49);
-	return s;
-}
-void loadPosition()
-{
-	int k = 0;
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			int n = board[i][j];
-			if (!n) continue;
-			int x = abs(n) - 1;
-			int y = n > 0 ? 1 : 0;
-			f[k].setTextureRect( IntRect(size*x, size*y, size, size));
-			f[k].setPosition(size*j, size*i);
-			k++;
-		}
-	}
-}
+#define SIZE 8
 
 int main()
 {
-	RenderWindow window(VideoMode(453, 453), "The Chess!");  //503
-	
-	Texture t1, t2;
-	t1.loadFromFile("images/figures.png");
-	t2.loadFromFile("images/board0.png");
+    while(1)
+    {
+        ofstream myfile("/home/ilya/Документы/firstlaba/hello.txt");
+        myfile << "Wecome you in my Chess GAME!\n\n";
+      //  myfile << "X - White\n- is black\n";
+        myfile << "   A     B     C     D     E     F     G     H\n\n";
+        
+        // for(int i = 1; i <= SIZE; i++)
+        //  {  
+        //     myfile << "   ";
+            for(int j = 1; j <= SIZE;j++)
+            {
+                if(j == 1)
+                myfile << "   r     n     b     q     k     b     n     r   " << j << endl;
 
-	Sprite s(t1);
-	Sprite sBoard(t2);
+                if(j > 2 && j < 7)
+                  myfile << "                                                 " << j << endl;
+                if(j == 2)
+                myfile << "   p     p     p     p     p     p     p     p   " << j << endl;
 
-	for (int i = 0; i < 32; i++)
-	{
-		f[i].setTexture(t1);
-	}
+                if(j == 7)
+                myfile << "   P     P     P     P     P     P     P     P   " << j << endl;
 
-	loadPosition();
+                if(j == 8)
+                myfile << "   R     N     B     Q     K     B     N     R   " << j << endl;
+                 
+            }
+       
+      //   }       
+         
+         
+         char c = cin.get();
+         if(c == 27)
+         {
+            myfile.close();
+            break;  
+         }     
 
-	bool isMove = false;
-	float dx = 0, dy = 0;
-	Vector2f oldPos, newPos;
-	std::string str;
-	int n = 0;
+    }
 
-	while (window.isOpen())
-	{
-		Vector2i pos = Mouse::getPosition(window);
-		Event e;
-		while (window.pollEvent(e))
-		{
-			if (e.type == Event::Closed)
-			{
-				window.close();
-			}
-			//////drag and drop///////
-			if (e.type == Event::MouseButtonPressed)
-				if(e.key.code == Mouse::Left)
-					for (int i = 0; i < 32; i++)
-					{
-						if (f[i].getGlobalBounds().contains(pos.x, pos.y))
-						{
-							isMove = true;
-							n = i;
-							dx = pos.x - f[i].getPosition().x;
-							dy = pos.y - f[i].getPosition().y;
-							oldPos = f[i].getPosition();
-						}
-					}
-			if (e.type == Event::MouseButtonReleased)
-				if (e.key.code == Mouse::Left)
-				{
-					isMove = false;
-					Vector2f p = f[n].getPosition() + Vector2f(size / 2, size / 2);
-					newPos = Vector2f(size*int(p.x/size), size*int(p.y/size));
-					str = toChessNote(oldPos) + toChessNote(newPos);
-					//std::cout<<str<<std::endl;
-					f[n].setPosition(newPos);
-				}
-					
-		}
-
-		if (isMove)
-		{
-			f[n].setPosition(pos.x - dx, pos.y - dy);
-		}
-
-	///////draw///////
-	window.clear();
-	window.draw(sBoard);
-	for (int i = 0; i < 32; i++)
-	{
-		window.draw(f[i]);
-	}
-	window.display();
-	}
-	return 0;
+    return 0;
 }
